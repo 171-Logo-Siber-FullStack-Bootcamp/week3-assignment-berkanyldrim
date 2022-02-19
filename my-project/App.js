dfe11a03579e70994ec827da1f89867bb1b69b9c
Berkan Yıldırım <berkanyldrim@gmail.com>
2022-02-19 14:09:33 +0300

    Week3: Login screen and logo added

--- my-project/App.js
+++ my-project/App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScrenn from './src/screens/login-screen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LoginScrenn></LoginScrenn>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: "1",
+    backgroundColor: '#D1D1D1',
+    flexDirection:'column',
+    justifyContent:'center',
+    // alignItems:'center'
+    
   },
 });
